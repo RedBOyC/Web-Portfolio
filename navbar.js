ba5493fad960c427de1f04455a3b8cededa9372b
RedboyZet135 <121138967+RedBOyC@users.noreply.github.com>
2024-02-01 08:50:20 +0800

    Add files via upload

---
+++ navbar.js
@@ -0,0 +1,9 @@
+// navbar.js
+document.write('<nav>\
+                    <a href="index.html">Home</a>\
+                    <a href="experiences.html">Experience</a>\
+                    <a href="achievements.html">Achievements</a>\
+                    <a href="projects.html">Projects</a>\
+                    <a href="research-papers.html">Research Papers</a>\
+                    <a href="links.html">Links</a>\
+                </nav>');
